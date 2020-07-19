bd655d9f3b14aeaf068c28be07557178b45df73c
Dimas Mokodompit <dimasdompit@gmail.com>
2020-07-19 23:22:41 +0700

    add dotenv, fix actions auth redux-persist

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
+import {API_URL} from '@env';
 
 export const login = (data) => {
   return {
     type: 'LOGIN',
     payload: axios({
       method: 'POST',
-      url: 'http://192.168.43.85:3000/auth/login',
+      url: `${API_URL}/auth/login`,
       data: {
         username: data.username,
         password: data.password,
